25751155752e2491d9d364a634fc258fe649716c
Michal Adámek <romster2012@seznam.cz>
2025-02-19 19:08:38 +0100

    user can see products and can click on them

---
+++ client/app/[id].tsx
@@ -0,0 +1,67 @@
+import { StyleSheet, Image, Platform, ActivityIndicator, Text} from 'react-native';
+
+import { useEffect, useState } from 'react';
+
+import { ThemedView } from '@/components/ThemedView';
+import { ThemedText } from '@/components/ThemedText';
+import { useLocalSearchParams } from 'expo-router';
+import {getProductById} from "./models/db_connects"
+
+type ProductType = {
+  _id: string,
+  title: string,
+  category: string,
+  description: string,
+  cost: number
+}
+
+export default function ProductView() {
+  const {id} = useLocalSearchParams<{ id:string }>()
+  const [products, setProduct] = useState<ProductType>();
+  const [isLoaded, setLoaded] = useState(false);
+
+  const load = async () => {
+      const data = await getProductById(id);
+      if (data.status === 500 || data.status === 404) {
+        setLoaded(false);
+        return;
+      }
+      if (data.status === 200) {
+        setProduct(data.payload);
+        setLoaded(true);
+      }
+      console.log(products)
+    };
+  
+    useEffect(() => {
+      load();
+    }, []);
+  
+  if (isLoaded === null) {
+    return <Text>Products not found</Text>;
+  }
+  
+  if (!isLoaded) {
+    return <ActivityIndicator size="large" color="#0000ff" />;
+  }
+
+  return (
+    <ThemedView style={styles.container}>
+      <ThemedText>
+        {products?._id}
+      </ThemedText>
+    </ThemedView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 16,
+    alignItems: "center",
+    justifyContent: "center",
+    flexWrap: "wrap",
+    flexDirection: "row",
+    gap: 16,
+    height: "100%",
+  },
+});
